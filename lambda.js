a56b749ee0b6e223592403b36fcb9cbeba243f97
nplutt <nplutt@gmail.com>
2017-01-30 22:01:50 -0600

    Working locally.

--- lambda.js
+++ lambda.js
@@ -1,40 +1,63 @@
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-var unzip = require('unzip');
-var stm = require('stream');
+var s3 = new AWS.S3({signatureVersion: 'v4'});
+var unzip = require('unzip2');
 var util = require('util');
 var fs = require('fs');
+var async = require('async');
 
 exports.handler = function (event, context) {
     var bucket = event.Records[0].s3.bucket.name;
     var key = event.Records[0].s3.object.key;
-    console.log('Bucket: ' + bucket + ' Key: ' + key);
-    console.log('Retriving file');
-    var params = {
-        Bucket: bucket,
-        Key: key
-    };
-    s3.getObject(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-        }
-        else {
-            console.log('Retrieved zip file ' + key);
-            var file = fs.createWriteStream('tmp.zip');
-            console.log(data.Body)
-            // data.Body.createReadStream().pipe(file);
-            // fs.createReadStream('./' + file)
-            //     .pipe(unzip.Parse())
-            //     .on('entry', function(entry) {
-            //         var fileName = entry.path;
-            //         entry.pipe(fs.createWriteStream('./'))
-            //     });
-            // fs.readdir(testFolder, function(err, files) {
-            //     files.forEach( function (f) {
-            //         console.log(file);
-            //     });
-            // });
-            // context.done(null, '');
-        }
-    });
+    unzipAndUpload(bucket, key);
 };
+
+function unzipAndUpload(bucket, key) {
+    var params = {
+        Key: key,
+        Bucket: bucket
+    };
+    
+    async.waterfall([
+        function(callback) {
+            s3.getObject(params, function(err, data) {
+                callback(err, data);
+            });
+        },
+        function(data, callback) {
+            fs.writeFile('tmp.zip', data.Body, function(err) {
+                callback(err);
+            });
+        },
+        function(callback) {
+            fs.mkdir('tmp', function(err) {
+                callback(err);
+            });
+        },
+        function(callback) {
+            var stream = fs.createReadStream('tmp.zip').pipe(unzip.Extract({ path: 'tmp' }));
+            stream.on('finish', function(err) {
+                callback(err)
+            });
+        },
+        function(callback) {
+            fs.readdir('./tmp/dist/', function(err, files) {
+                callback(err, files);
+            });
+        },
+        function(files, callback) {
+            async.each(files, function(fileName, callback) {
+                var file = fs.createReadStream('./tmp/dist/' + fileName);
+                var params = {
+                    Bucket: bucket,
+                    Key: fileName,
+                    Body: file
+                };
+                s3.putObject(params, function(err, data) {
+                    callback(err);
+                });
+            }, function(err) {
+                callback(err);
+            });
+        }
+    ]);
+}
--- muck.js
+++ muck.js
@@ -1,7 +1,6 @@
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-var unzip = require('unzip');
-var stm = require('stream');
+var s3 = new AWS.S3({signatureVersion: 'v4'});
+var unzip = require('unzip2');
 var util = require('util');
 var fs = require('fs');
 var async = require('async');
@@ -9,9 +8,8 @@ var async = require('async');
 getObject();
 
 function getObject() {
-    console.log('Hello world!');
     var bucket = 'nickplutt';
-    var key = 'ui-build-AngularPipe/MyAppBuild/muck.zip';
+    var key = 'ui-build-AngularPipe/MyAppBuild/apONXov';
 
     var params = {
         Key: key,
@@ -22,32 +20,44 @@ function getObject() {
     async.waterfall([
         function(callback) {
             s3.getObject(params, function(err, data) {
-                callback(err, data)
+                callback(err, data);
             });
         },
         function(data, callback) {
             fs.writeFile('tmp.zip', data.Body, function(err) {
+                callback(err);
+            });
+        },
+        function(callback) {
+            fs.mkdir('tmp', function(err) {
+                callback(err);
+            });
+        },
+        function(callback) {
+            var stream = fs.createReadStream('tmp.zip').pipe(unzip.Extract({ path: 'tmp' }));
+            stream.on('finish', function(err) {
                 callback(err)
-            })
+            });
+        },
+        function(callback) {
+            fs.readdir('./tmp/dist/', function(err, files) {
+                callback(err, files);
+            });
+        },
+        function(files, callback) {
+            async.each(files, function(fileName, callback) {
+                var file = fs.createReadStream('./tmp/dist/' + fileName);
+                var params = {
+                    Bucket: bucket,
+                    Key: fileName,
+                    Body: file
+                };
+                s3.putObject(params, function(err, data) {
+                    callback(err);
+                });
+            }, function(err) {
+               callback(err);
+            });
         }
     ]);
-    // s3.getObject(params, function(err, data) {
-    //     if (err) {
-    //         console.log(err)
-    //     }
-    //     else {
-    //         console.log('Retrieved the file!');
-    //         console.log(data.Body);
-    //
-    //         // var file = fs.createWriteStream('tmp.zip');
-    //         fs.writeFile('tmp.zip', data.Body, function (err) {
-    //             if (err) throw err;
-    //             console.log('It saved!');
-    //
-    //
-    //
-    //
-    //         });
-    //     }
-    // });
 }
