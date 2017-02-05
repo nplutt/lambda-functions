170550ea973458de88960a8a7b22a5517f31aa26
nplutt <nplutt@gmail.com>
2017-02-05 15:48:02 -0600

    Updated deploy to install claudia.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 # This script deploys the lambda to a specified region. Also adds a trigger for s3 bucket as 
 # well as a prefix to filter the prefix
 npm install
-npm update claudia
+npm install claudia
 claudia create --region $REGION --handler lambda.handler --timeout $TIMEOUT
 claudia add-s3-event-source --bucket $BUCKET --prefix $PREFIX
