f276515646eae03dd7daa2832cce3fc88d41c6af
nplutt <nplutt@gmail.com>
2017-02-10 22:12:20 -0600

    Updating deploy.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # well as a prefix to filter the prefix
 npm install
 npm install -g claudia
-claudia create --region $REGION --handler lambda.handler --timeout $TIMEOUT
+claudia create --region $REGION --handler lambda.handler --timeout $TIMEOUT --set-env BUCKET_NAME=$BUCKET_NAME
 claudia add-s3-event-source --bucket $BUCKET --prefix $PREFIX || true
